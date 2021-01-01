7781edd4d89ad420cdc351f3f3e59210b0b30ec1
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2021-01-01 11:38:51 -0600

    Adding nodes and relationship through app

--- app.js
+++ app.js
@@ -31,21 +31,152 @@ app.get('/', function(req, res){
             .then(function(result){
                 var personArr = [];    
                 result.records.forEach(function(record){
-                    //console.log(record._fields[0]);
+                
                     personArr.push({
                         id: record._fields[0].identity.low,
                         name: record._fields[0].properties.name 
                     })
                 });
-                res.render('index',
-                {
-                  persons: personArr  
+
+                session
+            .run("MATCH (n: Location) RETURN n")
+            .then(function(result){
+                var locationArr = [];    
+                result.records.forEach(function(record){
+                
+                    locationArr.push(
+                    record._fields[0].properties);
+                    });
+
+                    res.render('index',
+                        {
+                        persons: personArr ,
+                        locations: locationArr
+                        });
+
                 });
+
+                
             })
             .catch(function(error){
                 console.log(error);
             })
 })
+
+//Add Person Route
+app.post('/person/add', function(req, res){
+    var name = req.body.name;
+    session
+        .run("CREATE(n:Person {name:$nameParam}) RETURN n.name", {nameParam: name})
+        .then(function(result){
+            res.redirect('/');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
+});
+
+//Add Location Route
+app.post('/location/add', function(req, res){
+    var city = req.body.city;
+    var state = req.body.state;
+
+    session
+        .run("CREATE(n:Location {city:$cityParam}, {state:$stateParam}) RETURN n", {cityParam: city, stateParam: state})
+        .then(function(result){
+            res.redirect('/');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
+});
+
+//Add friends connect Route
+app.post('/friends/connect', function(req, res){
+    var name1 = req.body.name1;
+    var name2 = req.body.name2;
+
+    session
+        .run("MATCH(a: Person {name:$nameParam1}), (b: Person {name:$nameParam2}) MERGE(a)-[r:FRIENDS]->(b) RETURN a,b", 
+        {nameParam1: name1, nameParam2: name2})
+        .then(function(result){
+            res.redirect('/');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
+});
+
+//Add Birth location Route
+app.post('/person/born/add', function(req, res){
+    var name = req.body.name;
+    var city = req.body.city;
+    var state = req.body.state;
+    var year = req.body.year;
+
+    session
+        .run("MATCH(a: Person {name:$nameParam}), (b: Location {city:$cityParam, state:$stateParam}) MERGE(a)-[r:BORN_IN {year:$yearParam}]->(b) RETURN a,b", 
+        {nameParam: name, cityParam: city, stateParam: state, yearParam: year})
+        .then(function(result){
+            res.redirect('/');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
+});
+
+// Person route
+app.get('/person/:id', function(req, res){
+    var id = req.params.id;
+    
+    session
+        .run("MATCH(a:Person) WHERE id(a)=toInteger($idParam) RETURN a.name as name", {idParam: id})
+        .then(function(result){
+            var name = result.records[0].get("name");
+
+            session
+                .run("OPTIONAL MATCH (a:Person)-[r:BORN_IN]-(b:Location) WHERE id(a) = toInteger($idParam) RETURN b.city as city, b.state as state", {idParam: id})
+                .then(function(result2){
+                    var city = result2.records[0].get("city");
+                    var state = result2.records[0].get("state");
+
+                    session
+                        .run("OPTIONAL MATCH (a:Person)-[r:FRIENDS]-(b:Person) WHERE id(a)=toInteger($idParam) RETURN b", {idParam: id})
+                        .then(function(result3){
+                            var friendsArr = [];
+
+                            result3.records.forEach(function(record){
+                                if(record._fields[0] != null){
+                                    friendsArr.push({
+                                        id: record._fields[0].identity.low,
+                                        name: record._fields[0].properties.name
+                                    })
+                                }
+                            })
+                            res.render('person',{
+                                id:id,
+                                name:name,
+                                city:city,
+                                state:state,
+                                friends:friendsArr
+                            });
+                          
+                        })
+                        .catch(function(error){
+                            console.log(error);
+                        });
+                    });
+        });
+    })
+
 
 app.listen(3000);
 
